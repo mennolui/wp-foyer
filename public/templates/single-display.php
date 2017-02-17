d72a7d0606489d26b63276d44a815f33d42c2998
Menno Luitjes <menno@mennoluitjes.nl>
2017-02-17 16:05:20 +0100

    Tweaked the display template.

--- public/templates/single-display.php
+++ public/templates/single-display.php
@@ -1,7 +1,7 @@
-<?php	
+<?php
 /**
  * Display template.
- * 
+ *
  * @since	1.0.0
  */
 
@@ -12,22 +12,25 @@ global $post;
 		wp_head( );
 	?></head>
 	<body <?php body_class();?>>
-		<div class="channel"><?php	
-		
-			$display = new Foyer_Display( get_the_id() );
-			$channel = new Foyer_Channel( $display->get_active_channel() );
+		<div class="display">
+			<div class="channel">
+				<div class="slides"><?php
 
-			foreach( $channel->get_slides() as $slide ) {
-				
-				$post = get_post( $slide->ID );
-				setup_postdata( $post );
-				
-				Foyer_Templates::get_template('slides/'.$slide->format().'.php');
-				
-			}
-			
-			wp_reset_postdata();
-		?></div><?php
+					$display = new Foyer_Display( get_the_id() );
+					$channel = new Foyer_Channel( $display->get_active_channel() );
+
+					foreach( $channel->get_slides() as $slide ) {
+
+						$post = get_post( $slide->ID );
+						setup_postdata( $post );
+
+						Foyer_Templates::get_template('slides/'.$slide->format().'.php');
+					}
+
+					wp_reset_postdata();
+				?></div>
+			</div>
+		</div><?php
 		wp_footer();
 	?></body>
 </html>
