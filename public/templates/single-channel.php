34bbe0549712aeb4c50eaee809cf157834fc2785
Menno Luitjes <menno@mennoluitjes.nl>
2017-02-24 22:10:50 +0100

    Updated the single channel template to the single display template structure. Added a next class to the first slide in the templates for a gracious slide-in when the slideshow starts.

--- public/templates/single-channel.php
+++ public/templates/single-channel.php
@@ -7,23 +7,26 @@
 
 global $post;
 
+$channel = new Foyer_Channel( get_the_id() );
+
 ?><html>
 	<head><?php
 		wp_head( );
 	?></head>
 	<body <?php body_class();?>>
-		<div class="foyer-channel">
+		<div class="foyer-channel foyer-transition-slide foyer-channel-<?php echo $channel->ID; ?>">
 			<div class="foyer-slides"><?php
 
-				$channel = new Foyer_Channel( get_the_id() );
-
+				$first_slide_class = 'next';
 				foreach( $channel->get_slides() as $slide ) {
 
 					$post = get_post( $slide->ID );
 					setup_postdata( $post );
 
-					?><div class="foyer-slide foyer-slide-<?php echo $slide->format(); ?>"><?php
+					?><div class="foyer-slide foyer-slide-<?php echo $slide->format(); ?> <?php echo $first_slide_class; ?>"
+						data-foyer-slide-duration="2.5"><?php
 
+						$first_slide_class = '';
 						Foyer_Templates::get_template('slides/'.$slide->format().'.php');
 
 					?></div><?php
--- public/templates/single-display.php
+++ public/templates/single-display.php
@@ -19,13 +19,15 @@ $channel = new Foyer_Channel( $display->get_active_channel() );
 			<div class="foyer-channel foyer-transition-slide foyer-channel-<?php echo $channel->ID; ?>">
 				<div class="foyer-slides"><?php
 
+					$first_slide_class = 'next';
 					foreach( $channel->get_slides() as $slide ) {
 
 						$post = get_post( $slide->ID );
 						setup_postdata( $post );
+						?><div class="foyer-slide foyer-slide-<?php echo $slide->format(); ?> <?php echo $first_slide_class; ?>"
+							data-foyer-slide-duration="2.5"><?php
 
-						?><div class="foyer-slide foyer-slide-<?php echo $slide->format(); ?>" data-foyer-slide-duration="2.5"><?php
-
+							$first_slide_class = '';
 							Foyer_Templates::get_template('slides/'.$slide->format().'.php');
 
 						?></div><?php
