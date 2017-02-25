403acd567b095a17f674e4d0b3ddf86715bda19e
Menno Luitjes <menno@mennoluitjes.nl>
2017-02-25 21:42:32 +0100

    Added minimal styling to make slides cover the viewport. Added minimal styling for slide fields. Tweaked the slides templates.

--- public/templates/slides/default.php
+++ public/templates/slides/default.php
@@ -5,5 +5,7 @@
  * @since	1.0
  */
 ?><div class="inner">
-	<img src="<?php echo wp_get_attachment_url( get_post_meta( get_the_id(), 'slide_default_image', true ) ); ?>" />
+	<figure>
+		<img src="<?php echo wp_get_attachment_url( get_post_meta( get_the_id(), 'slide_default_image', true ) ); ?>" />
+	</figure>
 </div>
--- public/templates/slides/production.php
+++ public/templates/slides/production.php
@@ -8,6 +8,10 @@
 $production = new WPT_Production( get_post_meta( get_the_id(), 'slide_production_production_id', true ));
 
 ?><div class="inner">
-	<h1><?php echo $production->title(); ?></h1>
-	<img src="<?php echo wp_get_attachment_url( get_post_meta( get_the_id(), 'slide_production_image', true ) ); ?>" />
+	<figure>
+		<img src="<?php echo wp_get_attachment_url( get_post_meta( get_the_id(), 'slide_production_image', true ) ); ?>" />
+	</figure>
+	<div class="foyer-slide-fields">
+		<h1><?php echo $production->title(); ?></h1>
+	</div>
 </div>
