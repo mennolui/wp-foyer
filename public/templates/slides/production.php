1d998da17ab5d16aef3d2b352601c234359dc905
Jeroen Schmit <jeroen@slimndap.com>
2017-03-02 15:27:54 +0100

    Production slide now uses production featured image if no slide image is set.

--- public/templates/slides/production.php
+++ public/templates/slides/production.php
@@ -7,10 +7,26 @@
 
 $production = new WPT_Production( get_post_meta( get_the_id(), 'slide_production_production_id', true ));
 
+$slide_image = get_post_meta( get_the_id(), 'slide_production_image', true );
+
+if ( !empty( $slide_image ) ) {
+	$slide_image_url = wp_get_attachment_url( $slide_image );
+}
+
+// Fallback to production image if slide image is not set or not found.
+if ( empty( $slide_image_url ) ) {
+	$slide_image = $production->thumbnail();
+	if ( !empty( $slide_image ) ) {
+		$slide_image_url = wp_get_attachment_url( $slide_image );
+	}
+}
+
 ?><div class="inner">
-	<figure>
-		<img src="<?php echo wp_get_attachment_url( get_post_meta( get_the_id(), 'slide_production_image', true ) ); ?>" />
-	</figure>
+	<figure><?php
+		if (!empty( $slide_image_url )) {
+			?><img src="<?php echo $slide_image_url; ?>" /><?php
+		}		
+	?></figure>
 	<div class="foyer-slide-fields">
 		<h1><?php echo $production->title(); ?></h1>
 	</div>
