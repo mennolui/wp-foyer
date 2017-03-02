75945b41d93491f4e551ac25127e57a7b81711c4
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-02 17:44:06 +0100

    Included the jQuery Date and Time Picker js and css.

--- raw/js/admin/foyer-admin.js
+++ raw/js/admin/foyer-admin.js
@@ -1,3 +1,4 @@
+// @codekit-prepend "jquery.datetimepicker.js";
 // @codekit-prepend "foyer-admin-channel.js";
 // @codekit-prepend "foyer-admin-slide.js";
 // @codekit-prepend "foyer-admin-preview.js";
