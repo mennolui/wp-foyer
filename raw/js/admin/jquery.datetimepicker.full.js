6b2540386c82ac44f6aed83575c6f4b7977ad077
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-03 13:09:22 +0100

    Renamed the jQuery datetimepicker function to foyer_datetimepicker to avoid conflicts with other datetimepickers.

--- raw/js/admin/jquery.datetimepicker.full.js
+++ raw/js/admin/jquery.datetimepicker.full.js
@@ -1218,7 +1218,7 @@ var DateFormatter;
 	};
 
 	// for locale settings
-	$.datetimepicker = {
+	$.foyer_datetimepicker = {
 		setLocale: function(locale){
 			var newLocale = default_options.i18n[locale]?locale:globalLocaleDefault;
 			if(globalLocale != newLocale){
@@ -1431,7 +1431,7 @@ var DateFormatter;
 		});
 	};
 
-	$.fn.datetimepicker = function (opt, opt2) {
+	$.fn.foyer_datetimepicker = function (opt, opt2) {
 		var result = this,
 			KEY0 = 48,
 			KEY9 = 57,
@@ -2960,7 +2960,7 @@ var DateFormatter;
 		return result;
 	};
 
-	$.fn.datetimepicker.defaults = default_options;
+	$.fn.foyer_datetimepicker.defaults = default_options;
 
 	function HighlightedDate(date, desc, style) {
 		"use strict";
