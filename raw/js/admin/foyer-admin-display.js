60fb4cd3c8fba7d7721a092020b3aa4fe571bb5a
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-10 13:17:34 +0100

    Fix: Previews stopped working due to a Javascript error, datetimepicker setup was a bit too enthusiastic.

--- raw/js/admin/foyer-admin-display.js
+++ raw/js/admin/foyer-admin-display.js
@@ -10,28 +10,29 @@ function foyer_display_setup_channel_scheduler() {
 	$end_datetime = jQuery('#foyer_channel_editor_scheduled_channel_end');
 
 	if (jQuery($start_datetime).length || jQuery($end_datetime).length) {
-		// Only set when datetime picker fields are present
-		jQuery.foyer_datetimepicker.setLocale(foyer_channel_scheduler_defaults.locale);
-	}
 
-	$start_datetime.foyer_datetimepicker({
-		format: foyer_channel_scheduler_defaults.datetime_format,
-		dayOfWeekStart : foyer_channel_scheduler_defaults.start_of_week,
-		step: 15,
-		onChangeDateTime: function(start) {
-			if (start) {
-				if (!$end_datetime.val() || new Date($end_datetime.val()) < start) {
-					var new_end = new Date(start.getTime() + foyer_channel_scheduler_defaults.duration * 1000)
-					$end_datetime.val(new_end.dateFormat(foyer_channel_scheduler_defaults.datetime_format));
+		// Only continue when datetime picker fields are present, datetimepicker will work with empty jQuery objects
+		jQuery.foyer_datetimepicker.setLocale(foyer_channel_scheduler_defaults.locale);
+
+		$start_datetime.foyer_datetimepicker({
+			format: foyer_channel_scheduler_defaults.datetime_format,
+			dayOfWeekStart : foyer_channel_scheduler_defaults.start_of_week,
+			step: 15,
+			onChangeDateTime: function(start) {
+				if (start) {
+					if (!$end_datetime.val() || new Date($end_datetime.val()) < start) {
+						var new_end = new Date(start.getTime() + foyer_channel_scheduler_defaults.duration * 1000)
+						$end_datetime.val(new_end.dateFormat(foyer_channel_scheduler_defaults.datetime_format));
+					}
 				}
 			}
-		}
-	});
+		});
 
-	$end_datetime.foyer_datetimepicker({
-		format: foyer_channel_scheduler_defaults.datetime_format,
-		dayOfWeekStart : foyer_channel_scheduler_defaults.start_of_week,
-		step: 15
-	});
+		$end_datetime.foyer_datetimepicker({
+			format: foyer_channel_scheduler_defaults.datetime_format,
+			dayOfWeekStart : foyer_channel_scheduler_defaults.start_of_week,
+			step: 15
+		});
 
+	}
 }
