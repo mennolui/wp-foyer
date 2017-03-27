175b5a9a7c1cd2c3409217dd72b9b7c85186d995
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-28 00:42:56 +0200

    Added some code that loads the Youtube IFrame Player API, creates an iframe and starts playing when player is ready.

--- public/templates/slides/video.php
+++ public/templates/slides/video.php
@@ -9,6 +9,35 @@ $slide = new Foyer_Slide( get_the_id() );
 
 ?><div<?php $slide->classes(); ?><?php $slide->data_attr();?>>
 	<div class="inner">
-		<?php echo apply_filters( 'the_content', 'https://www.youtube.com/watch?v=r9tbusKyvMY' ); ?>
+		<div class="video" id="youtube-player-1"></div>
+
+
+	    <script>
+	      // 2. This code loads the IFrame Player API code asynchronously.
+	      var tag = document.createElement('script');
+
+	      tag.src = "https://www.youtube.com/iframe_api";
+	      var firstScriptTag = document.getElementsByTagName('script')[0];
+	      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	      // 3. This function creates an <iframe> (and YouTube player)
+	      //    after the API code downloads.
+	      var youtube_player_1;
+	      function onYouTubeIframeAPIReady() {
+	        youtube_player_1 = new YT.Player('youtube-player-1', {
+	          width: '1920',
+	          height: '1080',
+	          videoId: 'r9tbusKyvMY',
+	          events: {
+	            'onReady': onPlayerReady,
+	          }
+	        });
+	      }
+
+	      // 4. The API will call this function when the video player is ready.
+	      function onPlayerReady(event) {
+	        event.target.playVideo();
+	      }
+	    </script>
 	</div>
 </div>
