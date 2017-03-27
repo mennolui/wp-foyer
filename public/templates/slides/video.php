0fbcf6c801a79199393f3cceeb26aa7ccf68b7b7
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-28 00:51:12 +0200

    Added some Youtube player vars to minimize the visibility of the player UI and branding.

--- public/templates/slides/video.php
+++ public/templates/slides/video.php
@@ -12,32 +12,39 @@ $slide = new Foyer_Slide( get_the_id() );
 		<div class="video" id="youtube-player-1"></div>
 
 
-	    <script>
-	      // 2. This code loads the IFrame Player API code asynchronously.
-	      var tag = document.createElement('script');
+		<script>
+		// 2. This code loads the IFrame Player API code asynchronously.
+		var tag = document.createElement('script');
 
-	      tag.src = "https://www.youtube.com/iframe_api";
-	      var firstScriptTag = document.getElementsByTagName('script')[0];
-	      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-	      // 3. This function creates an <iframe> (and YouTube player)
-	      //    after the API code downloads.
-	      var youtube_player_1;
-	      function onYouTubeIframeAPIReady() {
-	        youtube_player_1 = new YT.Player('youtube-player-1', {
-	          width: '1920',
-	          height: '1080',
-	          videoId: 'r9tbusKyvMY',
-	          events: {
-	            'onReady': onPlayerReady,
-	          }
-	        });
-	      }
+		// 3. This function creates an <iframe> (and YouTube player)
+		//    after the API code downloads.
+		var youtube_player_1;
+		function onYouTubeIframeAPIReady() {
+			youtube_player_1 = new YT.Player('youtube-player-1', {
+				width: '1920',
+				height: '1080',
+				videoId: 'r9tbusKyvMY',
+				playerVars: {
+					'controls': 0,
+					'loop': 1,
+					'modestbranding': 1,
+					'rel': 0,
+					'showinfo': 0
+				},
+				events: {
+					'onReady': onPlayerReady,
+				}
+			});
+		}
 
-	      // 4. The API will call this function when the video player is ready.
-	      function onPlayerReady(event) {
-	        event.target.playVideo();
-	      }
-	    </script>
+		// 4. The API will call this function when the video player is ready.
+		function onPlayerReady(event) {
+			event.target.playVideo();
+		}
+		</script>
 	</div>
 </div>
