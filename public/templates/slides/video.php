44f3e32de4408850aaf545e8a2b29140bf9294f0
Menno Luitjes <menno@mennoluitjes.nl>
2017-03-29 00:10:56 +0200

    Added some experimental event listeners that play and pause the video when slide state changes to and from active.

--- public/templates/slides/video.php
+++ public/templates/slides/video.php
@@ -13,38 +13,49 @@ $slide = new Foyer_Slide( get_the_id() );
 
 
 		<script>
-		// 2. This code loads the IFrame Player API code asynchronously.
-		var tag = document.createElement('script');
+			// 2. This code loads the IFrame Player API code asynchronously.
+			var tag = document.createElement('script');
 
-		tag.src = "https://www.youtube.com/iframe_api";
-		var firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-		// 3. This function creates an <iframe> (and YouTube player)
-		//    after the API code downloads.
-		var youtube_player_1;
-		function onYouTubeIframeAPIReady() {
-			youtube_player_1 = new YT.Player('youtube-player-1', {
-				width: '1920',
-				height: '1080',
-				videoId: 'r9tbusKyvMY',
-				playerVars: {
-					'controls': 0,
-					'loop': 1,
-					'modestbranding': 1,
-					'rel': 0,
-					'showinfo': 0
-				},
-				events: {
-					'onReady': onPlayerReady,
-				}
-			});
-		}
+			// 3. This function creates an <iframe> (and YouTube player)
+			//    after the API code downloads.
+			var youtube_player_1;
+			function onYouTubeIframeAPIReady() {
+				youtube_player_1 = new YT.Player('youtube-player-1', {
+					width: '1920',
+					height: '1080',
+					videoId: 'r9tbusKyvMY',
+					playerVars: {
+						'controls': 0,
+						'loop': 1,
+						'modestbranding': 1,
+						'rel': 0,
+						'showinfo': 0
+					},
+					events: {
+						'onReady': onPlayerReady,
+					}
+				});
+			}
 
-		// 4. The API will call this function when the video player is ready.
-		function onPlayerReady(event) {
-			event.target.playVideo();
-		}
+			// 4. The API will call this function when the video player is ready.
+			function onPlayerReady(yt_event) {
+				// trigger buffering so video is ready to play when needed
+				yt_event.target.playVideo();
+				yt_event.target.pauseVideo();
+
+				jQuery('.foyer-slide-video').on('slide:add-active', function( event ) {
+					yt_event.target.playVideo();
+				});
+				jQuery('.foyer-slide-video').on('slide:remove-active', function( event ) {
+					setTimeout(function() {
+						yt_event.target.pauseVideo();
+					}, foyer_ticker_css_transition_duration);
+				});
+			}
 		</script>
 	</div>
 </div>
