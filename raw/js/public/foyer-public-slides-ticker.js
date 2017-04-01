784e7083c0e68a6ceb412ab78fde2d9bfad556d8
Menno Luitjes <menno@mennoluitjes.nl>
2017-04-01 22:35:09 +0200

    Rewrote the slides custom event handlers.

--- raw/js/public/foyer-public-slides-ticker.js
+++ raw/js/public/foyer-public-slides-ticker.js
@@ -2,30 +2,77 @@ var foyer_ticker_shutdown_status = false;
 var foyer_ticker_shutdown_callback;
 var foyer_ticker_shutdown_callback_options;
 
-var foyer_ticker_css_transition_duration = 1.5 * 1000; // (2 seconds in milliseconds)
-var foyer_ticker_css_transition_duration_safe = foyer_ticker_css_transition_duration + (.5 * 1000); // (add 0.5 seconds in milliseconds)
+var foyer_ticker_css_transition_duration = 1.5; // 1.5 seconds
+var foyer_ticker_css_transition_duration_safe = foyer_ticker_css_transition_duration + 0.5; // add 0.5 seconds
 
 jQuery(document).ready(function() {
 
 	if (jQuery(foyer_slides_selector).length) {
-		// Our view includes slides, setup ticker
-		foyer_ticker_setup();
+		// Our view includes slides, initialize ticker
+		foyer_ticker_init();
 	}
 
 });
 
+function foyer_ticker_bind_events() {
+	// Allow others to bind events before us, so they can prevent ours
+	jQuery(foyer_slides_selector).trigger('slides:before-binding-events');
 
-function foyer_ticker_setup() {
-	// become,became / leave/left
-	// entering,entered / leaving,left
-	// before/after
+	jQuery(foyer_slides_selector).on('slides:next-slide', function( event ) {
+		var $active_slide = jQuery(foyer_slide_selector + '.active');
+		var slide_count = jQuery(foyer_slide_selector).length;
+
+		var new_active_index = jQuery(foyer_slide_selector).index($active_slide) + 1;
+		if (new_active_index >= slide_count) {
+			new_active_index = 0;
+		}
+
+		var new_next_index = new_active_index + 1;
+		if (new_next_index >= slide_count) {
+			new_next_index = 0;
+		}
+
+		$active_slide.trigger('slide:leaving-active');
+
+		if (foyer_ticker_shutdown_status) {
+			foyer_ticker_shutdown_status = false;
+
+			// Trigger callback, but only after some time has passed to finish all CSS transitions
+			setTimeout(function() {
+				foyer_ticker_shutdown_callback(foyer_ticker_shutdown_callback_options);
+			}, foyer_ticker_css_transition_duration_safe * 1000);
+		}
+		else {
+
+			jQuery(foyer_slide_selector).eq(new_active_index).trigger('slide:becoming-active');
+			jQuery(foyer_slide_selector).eq(new_next_index).trigger('slide:becoming-next');
+			foyer_ticker_set_active_slide_timeout();
+		}
+	});
+
+	jQuery(foyer_slide_selector).on('slide:becoming-next', function( event ) {
+		jQuery(this).addClass('next').trigger('slide:became-next');
+	});
+	jQuery(foyer_slide_selector).on('slide:becoming-active', function( event ) {
+		jQuery(this).removeClass('next').addClass('active').trigger('slide:became-active');
+	});
+	jQuery(foyer_slide_selector).on('slide:leaving-active', function( event ) {
+		jQuery(this).removeClass('active').trigger('slide:left-active');
+	});
+
+	jQuery(foyer_slides_selector).trigger('slides:after-binding-events');
+}
+
+
+function foyer_ticker_init() {
+	foyer_ticker_bind_events();
 	foyer_ticker_set_slide_active_next_classes();
 	foyer_ticker_set_active_slide_timeout();
 }
 
 function foyer_ticker_set_slide_active_next_classes() {
-	jQuery(foyer_slide_selector).first().trigger('slide:remove-next').removeClass('next').trigger('slide:add-active').addClass('active');
-	jQuery(foyer_slide_selector).first().next().trigger('slide:add-next').addClass('next');
+	jQuery(foyer_slide_selector).first().trigger('slide:becoming-active');
+	jQuery(foyer_slide_selector).first().next().trigger('slide:becoming-next');
 }
 
 function foyer_ticker_set_active_slide_timeout() {
@@ -40,36 +87,7 @@ function foyer_ticker_set_active_slide_timeout() {
 }
 
 function foyer_ticker_next_slide() {
-
-	var $active_slide = jQuery(foyer_slide_selector + '.active');
-	var slide_count = jQuery(foyer_slide_selector).length;
-
-	var new_active_index = jQuery(foyer_slide_selector).index($active_slide) + 1;
-	if (new_active_index >= slide_count) {
-		new_active_index = 0;
-	}
-
-	var new_next_index = new_active_index + 1;
-	if (new_next_index >= slide_count) {
-		new_next_index = 0;
-	}
-
-	$active_slide.trigger('slide:remove-active').removeClass('active');
-
-	if (foyer_ticker_shutdown_status) {
-		foyer_ticker_shutdown_status = false;
-
-		// Trigger callback, but only after some time has passed to finish all CSS transitions
-		setTimeout(function() {
-			foyer_ticker_shutdown_callback(foyer_ticker_shutdown_callback_options);
-		}, foyer_ticker_css_transition_duration_safe);
-	}
-	else {
-
-		jQuery(foyer_slide_selector).eq(new_active_index).trigger('slide:remove-next').removeClass('next').trigger('slide:add-active').addClass('active');
-		jQuery(foyer_slide_selector).eq(new_next_index).trigger('slide:add-next').addClass('next');
-		foyer_ticker_set_active_slide_timeout();
-	}
+	jQuery(foyer_slides_selector).trigger('slides:next-slide');
 }
 
 function foyer_ticker_shutdown(callback, options) {
