4b4c9cd0977715fa966e30f65c55dfeb58676609
Menno Luitjes <menno@mennoluitjes.nl>
2017-04-09 23:07:42 +0200

    Added YouTube Video ID, start, end and wait-for-end fields to the video slide admin and video slide template. You can now choose what video should be played! Eg. Future Islands - Ran.

--- admin/class-foyer-admin-slide-format-video.php
+++ admin/class-foyer-admin-slide-format-video.php
@@ -20,6 +20,27 @@ class Foyer_Admin_Slide_Format_Video {
 	 * @return	void
 	 */
 	static function save_slide_video( $post_id ) {
+		$slide_video_video_id = sanitize_text_field( $_POST['slide_video_video_id'] );
+
+		$slide_video_video_start = intval( $_POST['slide_video_video_start'] );
+		if ( empty( $slide_video_video_start ) ) {
+			$slide_video_video_start = '';
+		}
+
+		$slide_video_video_end = intval( $_POST['slide_video_video_end'] );
+		if ( empty( $slide_video_video_end ) ) {
+			$slide_video_video_end = '';
+		}
+
+		$slide_video_video_wait_for_end = intval( $_POST['slide_video_video_wait_for_end'] );
+		if ( empty( $slide_video_video_wait_for_end ) ) {
+			$slide_video_video_wait_for_end = '';
+		}
+
+		update_post_meta( $post_id, 'slide_video_video_id', $slide_video_video_id );
+		update_post_meta( $post_id, 'slide_video_video_start', $slide_video_video_start );
+		update_post_meta( $post_id, 'slide_video_video_end', $slide_video_video_end );
+		update_post_meta( $post_id, 'slide_video_video_wait_for_end', $slide_video_video_wait_for_end );
 	}
 
 	/**
@@ -31,5 +52,50 @@ class Foyer_Admin_Slide_Format_Video {
 	 * @return	void
 	 */
 	static function slide_video_meta_box( $post ) {
+		$slide_video_video_id = get_post_meta( $post->ID, 'slide_video_video_id', true );
+		$slide_video_video_start = get_post_meta( $post->ID, 'slide_video_video_start', true );
+		$slide_video_video_end = get_post_meta( $post->ID, 'slide_video_video_end', true );
+		$slide_video_video_wait_for_end = get_post_meta( $post->ID, 'slide_video_video_wait_for_end', true );
+
+		?><table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row">
+						<label for="slide_video_video_id"><?php _e('YouTube video ID', 'foyer'); ?></label>
+					</th>
+					<td>
+						<input type="text" name="slide_video_video_id" id="slide_video_video_id"
+							value="<?php echo $slide_video_video_id; ?>" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="slide_video_video_start"><?php _e('Start at (seconds)', 'foyer'); ?></label>
+					</th>
+					<td>
+						<input type="text" name="slide_video_video_start" id="slide_video_video_start"
+							value="<?php echo $slide_video_video_start; ?>" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="slide_video_video_end"><?php _e('Stop at (seconds)', 'foyer'); ?></label>
+					</th>
+					<td>
+						<input type="text" name="slide_video_video_end" id="slide_video_video_end"
+							value="<?php echo $slide_video_video_end; ?>" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="slide_video_video_wait_for_end"><?php _e('Wait for end?', 'foyer'); ?></label>
+					</th>
+					<td>
+						<input type="checkbox" name="slide_video_video_wait_for_end" id="slide_video_video_wait_for_end"
+							value="1" <?php checked( $slide_video_video_wait_for_end, 1 ); ?> />
+					</td>
+				</tr>
+			</tbody>
+		</table><?php
 	}
 }
--- public/templates/slides/video.php
+++ public/templates/slides/video.php
@@ -7,13 +7,18 @@
 
 $slide = new Foyer_Slide( get_the_id() );
 
+$video_id = get_post_meta( get_the_id(), 'slide_video_video_id', true );
+$video_start = get_post_meta( get_the_id(), 'slide_video_video_start', true );
+$video_end = get_post_meta( get_the_id(), 'slide_video_video_end', true );
+$video_wait_for_end = get_post_meta( get_the_id(), 'slide_video_video_wait_for_end', true );
+
 ?><div<?php $slide->classes(); ?><?php $slide->data_attr();?>>
 	<div class="inner">
 		<div class="youtube-video-container" id="<?php echo uniqid(); ?>"
-			data-foyer-video-id="r9tbusKyvMY"
-			data-foyer-video-start="35"
-			data-foyer-video-end="60"
-			data-foyer-video-wait-for-end="1"
+			data-foyer-video-id="<?php echo $video_id; ?>"
+			data-foyer-video-start="<?php echo $video_start; ?>"
+			data-foyer-video-end="<?php echo $video_end; ?>"
+			data-foyer-video-wait-for-end="<?php echo $video_wait_for_end; ?>"
 		></div>
 	</div>
 </div>
