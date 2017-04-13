76c60adeabfd658ed3d01e59940cb4dbf690a3b4
Menno Luitjes <menno@mennoluitjes.nl>
2017-04-13 16:09:06 +0200

    Fixed an issue where the video preview in the admin would not work when editing an existing video slide. Improved handling of changed start and end fields when no valid video URL is entered. Improved handling of emptied video URL field.

--- raw/js/admin/foyer-admin-slide-video.js
+++ raw/js/admin/foyer-admin-slide-video.js
@@ -2,16 +2,23 @@ var foyer_yt_player;
 
 jQuery( function() {
 
-	// Validate and rewrite YouTube video URL, load preview.
+	if (jQuery('#slide_video_video_url').val().length) {
+		// YouTube video URL is set on load, validate it and load preview
+		foyer_admin_slide_video_validate_youtube_video_url();
+	}
+
 	jQuery('#slide_video_video_url').on('change', function() {
+		// Validate changed YouTube video URL and load preview
 		foyer_admin_slide_video_validate_youtube_video_url();
 	});
 
 	jQuery('#slide_video_video_start').on('change', function() {
+		// Update player with changed start time
 		foyer_admin_slide_video_update_youtube_video_preview();
 	});
 
 	jQuery('#slide_video_video_end').on('change', function() {
+		// Update player with changed end time
 		foyer_admin_slide_video_update_youtube_video_preview();
 	});
 
@@ -54,35 +61,46 @@ function onYouTubeIframeAPIReady() {
 }
 
 function foyer_admin_slide_video_update_youtube_video_preview() {
-	if (window.foyer_yt_player) {
-		var player = window.foyer_yt_player;
+	if (jQuery('#slide_video_video_id').val().length) {
+		// Video ID is set, update preview
+		if (window.foyer_yt_player) {
+			var player = window.foyer_yt_player;
 
-		var video_id = jQuery('#slide_video_video_id').val();
-		var start = jQuery('#slide_video_video_start').val();
-		var end = jQuery('#slide_video_video_end').val();
+			var video_id = jQuery('#slide_video_video_id').val();
+			var start = jQuery('#slide_video_video_start').val();
+			var end = jQuery('#slide_video_video_end').val();
 
-		if (video_id) {
-			player.mute();
-			player.loadVideoById( {videoId: video_id, startSeconds: start, endSeconds: end} );
+			if (video_id) {
+				player.mute();
+				player.loadVideoById( {videoId: video_id, startSeconds: start, endSeconds: end} );
+			}
 		}
-	}
-	else {
-		foyer_admin_load_youtube_api();
+		else {
+			foyer_admin_load_youtube_api();
+		}
 	}
 }
 
 function foyer_admin_slide_video_validate_youtube_video_url() {
 	var video_metadata = foyer_get_video_id(jQuery('#slide_video_video_url').val());
+	console.log(video_metadata);
 
 	if (video_metadata && video_metadata.id && 'youtube' == video_metadata.service) {
-		jQuery('#slide_video_video_url').val('https://youtu.be/' + video_metadata.id)
-		jQuery('#slide_video_video_id').val(video_metadata.id)
+		// Valid YouTube video URL, rewrite URL field and update the video preview
+		jQuery('#slide_video_video_url').val('https://youtu.be/' + video_metadata.id);
+		jQuery('#slide_video_video_id').val(video_metadata.id);
 
 		jQuery('#slide_video_video_url_notification').addClass('hidden');
 
 		foyer_admin_slide_video_update_youtube_video_preview();
 	}
 	else {
+		// Not a valid URL, pause video, empty video ID and show message
+		if (window.foyer_yt_player) {
+			var player = window.foyer_yt_player;
+			player.pauseVideo();
+		}
+		jQuery('#slide_video_video_id').val('');
 		jQuery('#slide_video_video_url_notification').removeClass('hidden');
 	}
 }
