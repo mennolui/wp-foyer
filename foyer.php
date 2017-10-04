115746578d7cce89cf38dc1b7a5fa6e1dd0f640a
Menno Luitjes <menno@mennoluitjes.nl>
2017-10-04 16:19:02 +0200

    Ready for 1.2.4 release.

--- foyer.php
+++ foyer.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Foyer - Digital Signage for WordPress
  * Plugin URI:        http://mennoluitjes.nl
  * Description:       Create slideshows and show them off on your networked displays.
- * Version:           1.2.3
+ * Version:           1.2.4
  * Author:            Menno Luitjes
  * Author URI:        http://mennoluitjes.nl
  * License:           GPL-3.0+
