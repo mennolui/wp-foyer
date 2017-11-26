46e2e98c698419d079c66d7cd47d6b04a6a19417
Menno Luitjes <menno@mennoluitjes.nl>
2017-11-26 15:48:11 +0100

    Added production slide format tests.

---
+++ tests/test-foyer-admin-slide-format-production.php
@@ -0,0 +1,33 @@
+<?php
+
+class Test_Foyer_Admin_Slide_Format_Production extends Foyer_UnitTestCase {
+
+	function test_are_all_production_slide_properties_saved() {
+
+		// Load Theater plugin
+		require dirname( dirname( __FILE__ ) ) . '/../../plugins/theatre/theater.php';
+
+		$this->assume_role( 'administrator' );
+
+		$production_id = '222';
+
+		/* Create image attachment */
+		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
+		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
+
+		$_POST[ Foyer_Slide::post_type_name.'_nonce' ] = wp_create_nonce( Foyer_Slide::post_type_name );
+		$_POST['slide_format'] = 'production';
+
+		$_POST['slide_production_production_id'] = $production_id;
+		$_POST['slide_production_image'] = $image_attachment_id;
+
+		$admin_slide = new Foyer_Admin_Slide( 'foyer', '9.9.9' );
+		$admin_slide->save_slide( $this->slide1 );
+
+		$actual = get_post_meta( $this->slide1, 'slide_production_production_id', true );
+		$this->assertEquals( $production_id, $actual );
+
+		$actual = get_post_meta( $this->slide1, 'slide_production_image', true );
+		$this->assertEquals( $image_attachment_id, $actual );
+	}
+}
---
+++ tests/test-foyer-public-templates-slides-production.php
@@ -0,0 +1,39 @@
+<?php
+
+class Test_Foyer_Public_Templates_Slides_Production extends Foyer_UnitTestCase {
+
+	function test_are_all_production_slide_properties_included_in_slide() {
+
+		// Load Theater plugin
+		require dirname( dirname( __FILE__ ) ) . '/../../plugins/theatre/theater.php';
+
+		$this->assume_role( 'administrator' );
+
+		$production_title = 'Superduperproduction';
+		$prod_args = array(
+			'post_type' => WPT_Production::post_type_name,
+			'post_title' => $production_title,
+		);
+
+		/* Create production */
+		$production_id = $this->factory->post->create( $prod_args );
+
+		/* Create image attachment */
+		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
+		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
+
+		update_post_meta( $this->slide1, 'slide_format', 'production' );
+		update_post_meta( $this->slide1, 'slide_production_production_id', $production_id );
+		update_post_meta( $this->slide1, 'slide_production_image', $image_attachment_id );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template('partials/slide.php');
+		$actual = ob_get_clean();
+
+		$this->assertContains( $production_title, $actual );
+		$this->assertRegExp( '/Kip-400x400.*\.jpg/', $actual );
+	}
+}
+
