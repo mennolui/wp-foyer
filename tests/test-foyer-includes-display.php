ddc1dea46df5e856bd3f88e537ff357b156e35c1
Menno Luitjes <menno@mennoluitjes.nl>
2017-12-06 21:40:14 +0100

    Added two more tests, testing the get_default_channel behaviour.

--- tests/test-foyer-includes-display.php
+++ tests/test-foyer-includes-display.php
@@ -37,14 +37,11 @@ class Test_Foyer_Display extends Foyer_UnitTestCase {
 		$this->assertEquals( $channel_id, $actual );
 	}
 
-	function test_is_default_channel_used_when_schedule_has_no_published_channel() {
-
-		$channel_title = 'Plain default channel';
+	function test_is_default_channel_used_when_schedule_has_not_published_channel() {
 
 		/* Create published channel */
 		$channel_args = array(
 			'post_type' => Foyer_Channel::post_type_name,
-			'post_title' => $channel_title,
 		);
 
 		$channel_1_id = $this->factory->post->create( $channel_args );
@@ -52,7 +49,6 @@ class Test_Foyer_Display extends Foyer_UnitTestCase {
 		/* Create trashed channel */
 		$channel_args = array(
 			'post_type' => Foyer_Channel::post_type_name,
-			'post_title' => $channel_title,
 			'post_status' => 'trash',
 		);
 
@@ -81,14 +77,11 @@ class Test_Foyer_Display extends Foyer_UnitTestCase {
 		$this->assertEquals( $channel_1_id, $actual );
 	}
 
-	function test_is_schedules_channel_used_when_schedule_has_published_channel() {
-
-		$channel_title = 'Plain default channel';
+	function test_is_scheduled_channel_used_when_schedule_has_published_channel() {
 
 		/* Create published channels */
 		$channel_args = array(
 			'post_type' => Foyer_Channel::post_type_name,
-			'post_title' => $channel_title,
 		);
 
 		$channel_1_id = $this->factory->post->create( $channel_args );
@@ -115,5 +108,54 @@ class Test_Foyer_Display extends Foyer_UnitTestCase {
 		$actual = $display->get_active_channel();
 
 		$this->assertEquals( $channel_2_id, $actual );
+	}
+
+	function test_is_default_channel_not_returned_when_channel_is_not_published() {
+
+		/* Create trashed channel */
+		$channel_args = array(
+			'post_type' => Foyer_Channel::post_type_name,
+			'post_status' => 'trash',
+		);
+
+		$channel_id = $this->factory->post->create( $channel_args );
+
+		/* Create display with our trashed channel as default */
+		$display_args = array(
+			'post_type' => Foyer_Display::post_type_name,
+		);
+
+		$display_id = $this->factory->post->create( $display_args );
+		add_post_meta( $display_id, Foyer_Channel::post_type_name, $channel_id );
+
+		$display = new Foyer_Display( $display_id );
+
+		$actual = $display->get_active_channel();
+
+		$this->assertEquals( false, $actual );
+	}
+
+	function test_is_default_channel_returned_when_channel_is_published() {
+
+		/* Create published channel */
+		$channel_args = array(
+			'post_type' => Foyer_Channel::post_type_name,
+		);
+
+		$channel_id = $this->factory->post->create( $channel_args );
+
+		/* Create display with our published channel as default */
+		$display_args = array(
+			'post_type' => Foyer_Display::post_type_name,
+		);
+
+		$display_id = $this->factory->post->create( $display_args );
+		add_post_meta( $display_id, Foyer_Channel::post_type_name, $channel_id );
+
+		$display = new Foyer_Display( $display_id );
+
+		$actual = $display->get_active_channel();
+
+		$this->assertEquals( $channel_id, $actual );
 	}
 }
