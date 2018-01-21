f860c7e98156cdc68d7a5a5be0eda79fe6cc00ec
Menno Luitjes <menno@mennoluitjes.nl>
2018-01-21 22:41:03 +0100

    Removed the slide_production_image field from the production slide format admin since background images are now handled by slide backgrounds. Removed unused foyer-admin-slide-video.js.

--- admin/class-foyer-admin-slide-format-production.php
+++ admin/class-foyer-admin-slide-format-production.php
@@ -17,6 +17,7 @@ class Foyer_Admin_Slide_Format_Production {
 	 * @since	1.0.0
 	 * @since	1.0.1	Improved validating & sanitizing of the user input.
 	 * @since	1.1.0	Moved here from Foyer_Theater, and changed to static.
+	 * @since	1.4.0	Removed saving of slide_production_image since background images are now handled by slide backgrounds.
 	 *
 	 * @param	int		$post_id	The ID of the post being saved.
 	 * @return	void
@@ -27,13 +28,7 @@ class Foyer_Admin_Slide_Format_Production {
 			$slide_production_production_id = '';
 		}
 
-		$slide_production_image = intval( $_POST['slide_production_image'] );
-		if ( empty( $slide_production_image ) ) {
-			$slide_production_image = '';
-		}
-
 		update_post_meta( $post_id, 'slide_production_production_id', $slide_production_production_id );
-		update_post_meta( $post_id, 'slide_production_image', $slide_production_image );
 	}
 
 	/**
@@ -44,6 +39,7 @@ class Foyer_Admin_Slide_Format_Production {
 	 * @since	1.1.0	Moved here from Foyer_Theater, and changed to static.
 	 * @since	1.2.6	Changed the displayed name from Production to Event, same terminology as in Theater for WordPress.
 	 * @since	1.3.1	Fixed two labels that pointed to a non-existent field slide_default_subtitle, via for.
+	 * @since	1.4.0	Removed the slide_production_image admin field since background images are now handled by slide backgrounds.
 	 *
 	 * @param	WP_Post	$post	The post of the current slide.
 	 * @return	void
@@ -51,8 +47,6 @@ class Foyer_Admin_Slide_Format_Production {
 	static function slide_production_meta_box( $post ) {
 
 		global $wp_theatre;
-
-		$slide_production_image = get_post_meta( $post->ID, 'slide_production_image', true );
 
 		?><table class="form-table">
 			<tbody>
@@ -68,22 +62,6 @@ class Foyer_Admin_Slide_Format_Production {
 								?><option value="<?php echo intval( $production->ID ); ?>" <?php selected( get_post_meta( $post->ID, 'slide_production_production_id', true ), $production->ID, true ); ?>><?php echo esc_html( $production->title() ); ?></option><?php
 							}
 						?></select>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="slide_production_image"><?php _e( 'Background image', 'foyer' ); ?></label>
-					</th>
-					<td>
-						<div class="slide_image_field<?php if ( empty( $slide_production_image ) ) { ?> empty<?php } ?>">
-							<div class="image-preview-wrapper">
-								<img class="slide_image_preview" src="<?php echo esc_url( wp_get_attachment_url( $slide_production_image ) ); ?>" height="100">
-							</div>
-
-							<input type="button" class="button slide_image_upload_button" value="<?php _e( 'Upload image', 'foyer' ); ?>" />
-							<input type="button" class="button slide_image_delete_button" value="<?php _e( 'Remove image', 'foyer' ); ?>" />
-							<input type="hidden" name="slide_production_image" class="slide_image_value" value='<?php echo intval( $slide_production_image ); ?>'>
-						</div>
 					</td>
 				</tr>
 			</tbody>
