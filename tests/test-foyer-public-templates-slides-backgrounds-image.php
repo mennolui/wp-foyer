ac827220ea87f4aa750121b6f2ffcb1a233348a7
Menno Luitjes <menno@mennoluitjes.nl>
2018-01-25 21:39:53 +0100

    Rewrote Test_Foyer_Public_Templates_Slides_Default to Test_Foyer_Public_Templates_Slides_Backgrounds_Image to work with slide backgrounds.

--- tests/test-foyer-public-templates-slides-backgrounds-image.php
+++ tests/test-foyer-public-templates-slides-backgrounds-image.php
@@ -1,8 +1,11 @@
 <?php
 
-class Test_Foyer_Public_Templates_Slides_Default extends Foyer_UnitTestCase {
+class Test_Foyer_Public_Templates_Slides_Backgrounds_Image extends Foyer_UnitTestCase {
 
-	function test_are_all_default_slide_properties_included_in_slide() {
+	/**
+	 * @since	1.4.0
+	 */
+	function test_are_all_slide_background_image_properties_included_in_slide() {
 
 		$this->assume_role( 'administrator' );
 
@@ -10,8 +13,9 @@ class Test_Foyer_Public_Templates_Slides_Default extends Foyer_UnitTestCase {
 		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
 		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
 
-		update_post_meta( $this->slide1, 'slide_format', 'default' );
-		update_post_meta( $this->slide1, 'slide_default_image', $image_attachment_id );
+		update_post_meta( $this->slide1, 'slide_format', '' );
+		update_post_meta( $this->slide1, 'slide_background', 'image' );
+		update_post_meta( $this->slide1, 'slide_bg_image_image', $image_attachment_id );
 
 		$this->go_to( get_permalink( $this->slide1 ) );
 
