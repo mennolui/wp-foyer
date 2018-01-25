862150ef1ae5bc2e29f9f3dc315f9c3581b35fa6
Menno Luitjes <menno@mennoluitjes.nl>
2018-01-25 20:42:04 +0100

    Fixed an occasional PHP fatal error in the production default background.

--- public/templates/slides/backgrounds/default-production.php
+++ public/templates/slides/backgrounds/default-production.php
@@ -10,19 +10,22 @@ $slide = new Foyer_Slide( get_the_id() );
 $production_id = get_post_meta( $slide->ID, 'slide_production_production_id', true );
 $production = new WPT_Production( $production_id );
 
-$production_attachment_id = $production->thumbnail();
+if ( ! empty( $production ) ) {
 
-if ( ! empty( $production_attachment_id ) ) {
+	$production_attachment_id = $production->thumbnail();
 
-	$production_attachment_src = wp_get_attachment_image_src( $production_attachment_id, 'foyer_fhd_square' );
+	if ( ! empty( $production_attachment_id ) ) {
 
-	if ( ! empty( $production_attachment_src[0] ) ) {
+		$production_attachment_src = wp_get_attachment_image_src( $production_attachment_id, 'foyer_fhd_square' );
 
-		?><div<?php $slide->background_classes(); ?><?php $slide->background_data_attr();?>>
-			<figure>
-				<img src="<?php echo $production_attachment_src[0]; ?>" />
-			</figure>
-		</div><?php
+		if ( ! empty( $production_attachment_src[0] ) ) {
 
+			?><div<?php $slide->background_classes(); ?><?php $slide->background_data_attr();?>>
+				<figure>
+					<img src="<?php echo $production_attachment_src[0]; ?>" />
+				</figure>
+			</div><?php
+
+		}
 	}
 }
