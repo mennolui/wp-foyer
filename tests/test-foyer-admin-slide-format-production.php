fcc465f3ad340696bab72547bf0c5721f32925a7
Menno Luitjes <menno@mennoluitjes.nl>
2018-01-25 21:27:13 +0100

    Updated Test_Foyer_Admin_Slide_Format_Production to work with slide backgrounds.

--- tests/test-foyer-admin-slide-format-production.php
+++ tests/test-foyer-admin-slide-format-production.php
@@ -4,6 +4,10 @@
  */
 class Test_Foyer_Admin_Slide_Format_Production extends Foyer_UnitTestCase {
 
+	/**
+	 * @since	1.?
+	 * @since	1.4.0	Updated to work with slide backgrounds.
+	 */
 	function test_are_all_production_slide_properties_saved() {
 
 		// Load Theater plugin (if not loaded already)
@@ -13,22 +17,16 @@ class Test_Foyer_Admin_Slide_Format_Production extends Foyer_UnitTestCase {
 
 		$production_id = '222';
 
-		/* Create image attachment */
-		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
-		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
 
 		$_POST[ Foyer_Slide::post_type_name.'_nonce' ] = wp_create_nonce( Foyer_Slide::post_type_name );
 		$_POST['slide_format'] = 'production';
+		$_POST['slide_background'] = '';
 
 		$_POST['slide_production_production_id'] = $production_id;
-		$_POST['slide_production_image'] = $image_attachment_id;
 
 		Foyer_Admin_Slide::save_slide( $this->slide1 );
 
 		$actual = get_post_meta( $this->slide1, 'slide_production_production_id', true );
 		$this->assertEquals( $production_id, $actual );
-
-		$actual = get_post_meta( $this->slide1, 'slide_production_image', true );
-		$this->assertEquals( $image_attachment_id, $actual );
 	}
 }
