5d6a1d3919c33062b4ce4bb3d4f10855b37af619
Menno Luitjes <menno@mennoluitjes.nl>
2018-01-25 23:21:07 +0100

    Added a test that confirms the default background properties of a production slide are included in the slide.

---
+++ tests/test-foyer-public-templates-slides-backgrounds-default-production.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @group theater
+ */
+class Test_Foyer_Public_Templates_Slides_Backgrounds_Default_Production extends Foyer_UnitTestCase {
+
+	/**
+	 * @since	1.4.0
+	 */
+	function test_are_all_default_background_properties_of_production_slide_included_in_slide() {
+
+		// Load Theater plugin (if not loaded already)
+		require_once dirname( dirname( __FILE__ ) ) . '/../../plugins/theatre/theater.php';
+
+		$this->assume_role( 'administrator' );
+
+		$production_title = 'Superduperproductionwiththumb';
+		$prod_args = array(
+			'post_type' => WPT_Production::post_type_name,
+			'post_title' => $production_title,
+		);
+
+		/* Create production */
+		$production_id = $this->factory->post->create( $prod_args );
+
+		/* Create image attachment and set as production thumbnail */
+		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
+		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
+		set_post_thumbnail( $production_id, $image_attachment_id );
+
+		update_post_meta( $this->slide1, 'slide_format', 'production' );
+		update_post_meta( $this->slide1, 'slide_background', 'default' );
+		update_post_meta( $this->slide1, 'slide_production_production_id', $production_id );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template('partials/slide.php');
+		$actual = ob_get_clean();
+
+		$this->assertRegExp( '/Kip-400x400.*\.jpg/', $actual );
+	}
+}
+
