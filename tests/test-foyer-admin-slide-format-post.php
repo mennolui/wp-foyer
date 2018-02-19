74f731ef7c6b6ddf83bb9caff8c266c3dda61c03
Menno Luitjes <menno@mennoluitjes.nl>
2018-02-19 19:49:05 +0100

    Added some tests that confirm all is well with saving and using the extra Post slide format settings fields.

--- tests/test-foyer-admin-slide-format-post.php
+++ tests/test-foyer-admin-slide-format-post.php
@@ -9,16 +9,26 @@ class Test_Foyer_Admin_Slide_Format_Post extends Foyer_UnitTestCase {
 		$this->assume_role( 'administrator' );
 
 		$post_id = '222';
+		$slide_post_display_thumbnail = '1';
+		$slide_post_use_excerpt = '1';
 
 		$_POST[ Foyer_Slide::post_type_name.'_nonce' ] = wp_create_nonce( Foyer_Slide::post_type_name );
 		$_POST['slide_format'] = 'post';
 		$_POST['slide_background'] = 'default';
 
 		$_POST['slide_post_post_id'] = $post_id;
+		$_POST['slide_post_display_thumbnail'] = $slide_post_display_thumbnail;
+		$_POST['slide_post_use_excerpt'] = $slide_post_use_excerpt;
 
 		Foyer_Admin_Slide::save_slide( $this->slide1 );
 
 		$actual = get_post_meta( $this->slide1, 'slide_post_post_id', true );
 		$this->assertEquals( $post_id, $actual );
+
+		$actual = get_post_meta( $this->slide1, 'slide_post_display_thumbnail', true );
+		$this->assertEquals( $slide_post_display_thumbnail, $actual );
+
+		$actual = get_post_meta( $this->slide1, 'slide_post_use_excerpt', true );
+		$this->assertEquals( $slide_post_use_excerpt, $actual );
 	}
 }
--- tests/test-foyer-public-templates-slides-post.php
+++ tests/test-foyer-public-templates-slides-post.php
@@ -4,12 +4,14 @@ class Test_Foyer_Public_Templates_Slides_Post extends Foyer_UnitTestCase {
 	/**
 	 * @since	1.5.0
 	 */
-	function test_are_all_post_slide_properties_included_in_slide() {
+	function test_are_all_default_post_slide_properties_included_in_slide() {
 
 		$post_title = 'Hello world this is our post';
+		$post_content = 'With a lot a lot a lot a lot a lot of content.';
 		$args = array(
 			'post_type' => 'post',
 			'post_title' => $post_title,
+			'post_content' => $post_content,
 		);
 
 		/* Create post */
@@ -22,13 +24,111 @@ class Test_Foyer_Public_Templates_Slides_Post extends Foyer_UnitTestCase {
 		$this->go_to( get_permalink( $this->slide1 ) );
 
 		ob_start();
-		Foyer_Templates::get_template('partials/slide.php');
+		Foyer_Templates::get_template( 'partials/slide.php' );
 		$actual = ob_get_clean();
 
 		$this->assertContains( $post_title, $actual );
 		$this->assertContains( date_i18n( get_option( 'date_format' ) ), $actual );
+		$this->assertContains( $post_content, $actual );
+	}
 
-		// @todo: featured image
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_thumbnail_included_in_post_slide_with_display_thumbnail_set() {
+
+		$post_title = 'Hello world this is our post';
+		$args = array(
+			'post_type' => 'post',
+			'post_title' => $post_title,
+		);
+
+		/* Create post */
+		$post_id = $this->factory->post->create( $args );
+
+		/* Create image attachment */
+		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
+		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
+		set_post_thumbnail( $post_id, $image_attachment_id );
+
+		update_post_meta( $this->slide1, 'slide_format', 'post' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+		update_post_meta( $this->slide1, 'slide_post_post_id', $post_id );
+		update_post_meta( $this->slide1, 'slide_post_display_thumbnail', '1' );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertRegExp( '/Kip-400x400.*\.jpg/', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_thumbnail_not_included_in_post_slide_with_display_thumbnail_not_set() {
+
+		$post_title = 'Hello world this is our post';
+		$args = array(
+			'post_type' => 'post',
+			'post_title' => $post_title,
+		);
+
+		/* Create post */
+		$post_id = $this->factory->post->create( $args );
+
+		/* Create image attachment */
+		$file = dirname( __FILE__ ) . '/assets/Kip-400x400.jpg';
+		$image_attachment_id = $this->factory->attachment->create_upload_object( $file );
+		set_post_thumbnail( $post_id, $image_attachment_id );
+
+		update_post_meta( $this->slide1, 'slide_format', 'post' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+		update_post_meta( $this->slide1, 'slide_post_post_id', $post_id );
+		update_post_meta( $this->slide1, 'slide_post_display_thumbnail', '' );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertNotRegExp( '/Kip-400x400.*\.jpg/', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_excerpt_included_in_post_slide_with_use_excerpt_set() {
+
+		$post_title = 'Hello world this is our post';
+		$post_content = 'With a lot a lot a lot a lot a lot of content.';
+		$post_excerpt = 'Or a tiny excerpt.';
+		$args = array(
+			'post_type' => 'post',
+			'post_title' => $post_title,
+			'post_content' => $post_content,
+			'post_excerpt' => $post_excerpt,
+		);
+
+		/* Create post */
+		$post_id = $this->factory->post->create( $args );
+
+		update_post_meta( $this->slide1, 'slide_format', 'post' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+		update_post_meta( $this->slide1, 'slide_post_post_id', $post_id );
+		update_post_meta( $this->slide1, 'slide_post_use_excerpt', '1' );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertContains( $post_excerpt, $actual );
+		$this->assertNotContains( $post_content, $actual );
 	}
 }
 
