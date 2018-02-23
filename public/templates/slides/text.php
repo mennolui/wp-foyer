94d0cf57216ca61767a0659821a6e4356fcdc191
Menno Luitjes <menno@mennoluitjes.nl>
2018-02-23 21:17:09 +0100

    Added a template for the Text slide format, and tests that confirm all fields and enclosing HTML elements are displayed, or left out when field empty.

---
+++ public/templates/slides/text.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Text slide format template.
+ *
+ * @since	1.5.0
+ */
+
+$slide = new Foyer_Slide( get_the_id() );
+
+$slide_text_title = get_post_meta( $slide->ID, 'slide_text_title', true );
+$slide_text_subtitle = get_post_meta( $slide->ID, 'slide_text_subtitle', true );
+$slide_text_content = get_post_meta( $slide->ID, 'slide_text_content', true );
+
+?><div<?php $slide->classes(); ?><?php $slide->data_attr(); ?>>
+	<div class="inner">
+		<div class="foyer-slide-fields">
+			<?php if ( ! empty( $slide_text_title ) ) { ?>
+				<div class="foyer-slide-field foyer-slide-field-title"><span><?php echo $slide_text_title; ?></span></div>
+			<?php } ?>
+			<?php if ( ! empty( $slide_text_subtitle ) ) { ?>
+				<div class="foyer-slide-field foyer-slide-field-subtitle"><span><?php echo $slide_text_subtitle; ?></span></div>
+			<?php } ?>
+			<?php if ( ! empty( $slide_text_content ) ) { ?>
+				<div class="foyer-slide-field foyer-slide-field-content"><?php echo wpautop( $slide_text_content ); ?></div>
+			<?php } ?>
+		</div>
+	</div>
+	<?php $slide->background(); ?>
+</div>
---
+++ tests/test-foyer-public-templates-slides-text.php
@@ -0,0 +1,133 @@
+<?php
+class Test_Foyer_Public_Templates_Slides_Post extends Foyer_UnitTestCase {
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_are_all_text_slide_properties_included_in_slide() {
+
+		$title = 'Great title.';
+		$subtitle = 'Best subtitle.';
+		$content_line1 = '<strong>Some strong words.</strong>';
+		$content_line2 = 'And a second paragraph.';
+		$content = $content_line1 . "\n\n" . $content_line2;
+
+		update_post_meta( $this->slide1, 'slide_format', 'text' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+
+		update_post_meta( $this->slide1, 'slide_text_title', $title );
+		update_post_meta( $this->slide1, 'slide_text_subtitle', $subtitle );
+		update_post_meta( $this->slide1, 'slide_text_content', $content );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertContains( $title, $actual );
+		$this->assertContains( $subtitle, $actual );
+		$this->assertContains( '<p>' . $content_line1 . '</p>', $actual );
+		$this->assertContains( '<p>' . $content_line2 . '</p>', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_are_all_text_slide_field_elements_included_in_slide() {
+
+		$title = 'Great title.';
+		$subtitle = 'Best subtitle.';
+		$content = 'Strong content.';
+
+		update_post_meta( $this->slide1, 'slide_format', 'text' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+
+		update_post_meta( $this->slide1, 'slide_text_title', $title );
+		update_post_meta( $this->slide1, 'slide_text_subtitle', $subtitle );
+		update_post_meta( $this->slide1, 'slide_text_content', $content );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertContains( '<div class="foyer-slide-field foyer-slide-field-title', $actual );
+		$this->assertContains( '<div class="foyer-slide-field foyer-slide-field-subtitle', $actual );
+		$this->assertContains( '<div class="foyer-slide-field foyer-slide-field-content', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_title_element_not_included_in_text_slide_when_empty() {
+		$title = '';
+		$subtitle = 'Best subtitle.';
+		$content = 'Strong content.';
+
+		update_post_meta( $this->slide1, 'slide_format', 'text' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+
+		update_post_meta( $this->slide1, 'slide_text_title', $title );
+		update_post_meta( $this->slide1, 'slide_text_subtitle', $subtitle );
+		update_post_meta( $this->slide1, 'slide_text_content', $content );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertNotContains( '<div class="foyer-slide-field foyer-slide-field-title', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_subtitle_element_not_included_in_text_slide_when_empty() {
+		$title = 'Great title.';
+		$subtitle = '';
+		$content = 'Strong content.';
+
+		update_post_meta( $this->slide1, 'slide_format', 'text' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+
+		update_post_meta( $this->slide1, 'slide_text_title', $title );
+		update_post_meta( $this->slide1, 'slide_text_subtitle', $subtitle );
+		update_post_meta( $this->slide1, 'slide_text_content', $content );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertNotContains( '<div class="foyer-slide-field foyer-slide-field-subtitle', $actual );
+	}
+
+	/**
+	 * @since	1.5.0
+	 */
+	function test_is_content_element_not_included_in_text_slide_when_empty() {
+		$title = 'Great title.';
+		$subtitle = 'Best subtitle.';
+		$content = '';
+
+		update_post_meta( $this->slide1, 'slide_format', 'text' );
+		update_post_meta( $this->slide1, 'slide_background', '' );
+
+		update_post_meta( $this->slide1, 'slide_text_title', $title );
+		update_post_meta( $this->slide1, 'slide_text_subtitle', $subtitle );
+		update_post_meta( $this->slide1, 'slide_text_content', $content );
+
+		$this->go_to( get_permalink( $this->slide1 ) );
+
+		ob_start();
+		Foyer_Templates::get_template( 'partials/slide.php' );
+		$actual = ob_get_clean();
+
+		$this->assertNotContains( '<div class="foyer-slide-field foyer-slide-field-content', $actual );
+	}
+}
+
