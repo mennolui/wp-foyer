84b29848e1f4125547d5a9c7ed47f6010aa5ef1d
Menno Luitjes <menno@mennoluitjes.nl>
2018-03-12 20:04:07 +0100

    Removed Dutch translation files. Translations are now fully handled by https://translate.wordpress.org/projects/wp-plugins/foyer. Added a reminder to self about removing the Foyer_i18n class.

--- includes/class-foyer-i18n.php
+++ includes/class-foyer-i18n.php
@@ -6,8 +6,13 @@
  * Loads and defines the internationalization files for this plugin
  * so that it is ready for translation.
  *
+ * This class can be removed as soon as the Requires at least: field in the readme.txt is set to 4.6.
+ * See: https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/#loading-text-domain
+ *
  * @since		1.0.0
  * @since		1.3.2	Refactored class from object to static methods.
+ * @since		1.5.2	Removed Dutch translation files. Translations are now fully handled by
+ *						https://translate.wordpress.org/projects/wp-plugins/foyer.
  *
  * @package		Foyer
  * @subpackage	Foyer/includes
