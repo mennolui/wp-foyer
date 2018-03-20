7325bb5fc1044d8da118075bf92daf81ad80b4b0
Menno Luitjes <menno@mennoluitjes.nl>
2018-03-20 20:36:16 +0100

    Flushed the rewrite rules on activation (and deactivation) to make sure /foyer/ pretty permalinks for Displays work properly after plugin is freshly installed. Fixes #19.

--- includes/class-foyer-activator.php
+++ includes/class-foyer-activator.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * Fired during plugin activation.
- *
- * This class defines all code necessary to run during the plugin's activation.
+ * The class that holds all activation functionality.
  *
  * @since		1.0.0
  * @package		Foyer
@@ -13,14 +11,22 @@
 class Foyer_Activator {
 
 	/**
-	 * Short Description. (use period)
+	 * Does some housekeeping at plugin activation.
 	 *
-	 * Long Description.
+	 * Fired during plugin activation. Though when network activated only for the primary site.
 	 *
-	 * @since    1.0.0
+	 * @since	1.0.0
+	 * @since	1.5.3	Flushes the rewrite rules to make sure pretty permalinks for our custom post types
+	 *					work properly after plugin is activated. Fixes #19.
+	 *
+	 * @return	void
 	 */
 	public static function activate() {
 
-	}
+		// Make sure our custom post types are registered
+		Foyer_Setup::register_post_types();
 
+		// Re-build rewrite rules, including those for our custom post types
+		flush_rewrite_rules();
+	}
 }
--- includes/class-foyer-deactivator.php
+++ includes/class-foyer-deactivator.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * Fired during plugin deactivation.
- *
- * This class defines all code necessary to run during the plugin's deactivation.
+ * The class that holds all deactivation functionality.
  *
  * @since		1.0.0
  * @package		Foyer
@@ -13,14 +11,19 @@
 class Foyer_Deactivator {
 
 	/**
-	 * Short Description. (use period)
+	 * Does some housekeeping at plugin deactivation.
 	 *
-	 * Long Description.
+	 * Fired during plugin deactivation. Though when network activated only for the primary site.
 	 *
-	 * @since    1.0.0
+	 * @since	1.0.0
+	 * @since	1.5.3	Flushes the rewrite rules to make sure our rewrite rules are removed.
+	 *
+	 * @return	void
 	 */
 	public static function deactivate() {
 
+		// Our custom post types are not registered at this point
+		// Re-building rewrite rules, excluding those for our custom post types
+		flush_rewrite_rules();
 	}
-
 }
