ac27058c47759dcb54c63e3dad3807aa3f81ab1b
Menno Luitjes <menno@mennoluitjes.nl>
2018-11-13 22:54:38 +0100

    Moved a test that was individually tagged theater to a separate test class that extends the new Foyer_Theater_UnitTestCase (and thus includes the Theater plugin file). However, added this new test class within the same file.

--- tests/test-foyer-includes-slide-formats.php
+++ tests/test-foyer-includes-slide-formats.php
@@ -79,3 +79,20 @@ class Test_Foyer_Slide_Formats extends Foyer_UnitTestCase {
 		$this->assertNotEmpty( $slide_format );
 	}
 }
+
+/**
+ * @group theater
+ *
+ * Excluded when running phpunit by default. Use 'phpunit --group theater' to test.
+ */
+class Test_Foyer_Slide_Formats_Theater extends Foyer_Theater_UnitTestCase {
+
+	/**
+	 * @since	1.?
+	 */
+	function test_is_production_slide_format_registered_when_theater_is_active() {
+
+		$slide_format = Foyer_Slides::get_slide_format_by_slug( 'production' );
+		$this->assertNotEmpty( $slide_format );
+	}
+}
