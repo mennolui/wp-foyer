89e39b7b17c12eed3ed5c8b20f4594070df1ffb2
Menno Luitjes <menno@mennoluitjes.nl>
2019-03-15 14:46:47 +0100

    Added a trigger 'slides:removing-old-slide-group' that is fired just before a slide group is removed. Added the slide group class as parameter to the 'slides:removing-old-slide-group' and 'slides:loaded-new-slide-group' triggers, so these slide groups can be selectively targeted.

--- raw/js/public/foyer-public-display.js
+++ raw/js/public/foyer-public-display.js
@@ -31,6 +31,14 @@ function foyer_display_setup() {
 	foyer_loader_intervalObject = window.setInterval(foyer_display_load_data, 5 * 60 * 1000) // (5 minutes in milliseconds)
 }
 
+/**
+ * Loads and processes new HTML for the current display.
+ *
+ * @since	1.?.?
+ * @since	1.X.X	Added a trigger 'slides:removing-old-slide-group' that is fired just before a slide group is removed.
+ *					Added the slide group class as parameter to the 'slides:removing-old-slide-group'
+ *					and 'slides:loaded-new-slide-group' triggers, so these slide groups can be selectively targeted.
+ */
 function foyer_display_load_data() {
 	var current_slide_group_class;
 	var next_slide_group_class;
@@ -75,8 +83,10 @@ function foyer_display_load_data() {
 				) {
 					// Only one slide currently & one slide new slide
 					// Replace current slide with new slide from loaded HTML
-					jQuery(foyer_slides_selector).html($new_slides)
-						.trigger('slides:loaded-new-slide-group')
+					jQuery(foyer_slides_selector)
+						.trigger('slides:removing-old-slide-group', current_slide_group_class)
+						.html($new_slides)
+						.trigger('slides:loaded-new-slide-group', next_slide_group_class)
 						.trigger('slides:removed-old-slide-group');
 					foyer_ticker_set_slide_active_next_classes();
 				}
@@ -84,11 +94,12 @@ function foyer_display_load_data() {
 					// More than one slide currently, or one slide currently but more new slides
 					// Add new slides from loaded HTML to next slide group
 					jQuery(foyer_slides_selector).children().last().after($new_slides);
-					jQuery(foyer_slides_selector).trigger('slides:loaded-new-slide-group');
+					jQuery(foyer_slides_selector).trigger('slides:loaded-new-slide-group', next_slide_group_class);
 
 					jQuery('body').one('slide:left-active', '.'+next_slide_group_class, function( event ) {
 						// Ticker has advanced into the next group, and one of its slides (the first) has left active
 						// Empty the current (now previous) group to allow loading of fresh content
+						jQuery(foyer_slides_selector).trigger('slides:removing-old-slide-group', current_slide_group_class);
 						jQuery(foyer_slides_selector).find('.'+current_slide_group_class).remove();
 						jQuery(foyer_slides_selector).trigger('slides:removed-old-slide-group');
 						return true;
