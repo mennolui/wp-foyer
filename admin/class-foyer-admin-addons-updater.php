794730e32bd46612de7ba88490f5c9f4b04ab836
Menno Luitjes <menno@mennoluitjes.nl>
2019-04-01 17:05:14 +0200

    Documented EDD advice regarding the update class name.

--- admin/class-foyer-admin-addons-updater.php
+++ admin/class-foyer-admin-addons-updater.php
@@ -16,6 +16,11 @@ class Foyer_Admin_Addons_Updater {
 	 *
 	 * The EDD Plugin Updater periodically checks for valid license keys and available updates.
 	 *
+	 * Recommended by EDD: As more and more plugin developers use the Software Licensing add-on to deliver
+	 * updates to their products, there is a risk of a 'conflict' in another plugin including a different version
+	 * of the EDD_SL_Plugin_Updater class than you are packaging with your plugin. If you choose to, you can rename
+	 * this class to something unique to your plugin, so that it avoids conflicts.
+	 *
 	 * @since	1.X.X
 	 *
 	 * @return 	void
